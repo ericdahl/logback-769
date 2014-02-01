c1d0f147a83dff0110a309b3d025a7221183d836
Eric Dahl <eric.c.dahl@gmail.com>
2014-01-31 19:16:13 -0600

    initial

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String args[]) {
+        LOGGER.info("message 1");
+        LOGGER.info("message 2");
+        LOGGER.info("message 3");
+    }
+}
